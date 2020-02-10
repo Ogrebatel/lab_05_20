bf00293aade22ad9bdc882269232e32278e51d23
Ogrebatel <kotkov.oleg.2011@yandex.ru>
2020-02-10 15:24:01 +0300

    0.6.3

--- tests/test.cpp
+++ tests/test.cpp
@@ -15,19 +15,19 @@ public:
 };
 
 TEST(stack, Text){
-	stack<int> test;
+    stack<int> test;
     int a = 2, b = 3, c = 4;
 
     test.push(a);
     test.push(b);
     test.push(c);
     test.push(12);
-	
-	EXPECT_EQ(test.head(), 12);
+
+    EXPECT_EQ(test.head(), 12);
     EXPECT_EQ(test.pop(), 12);
-	EXPECT_EQ(test.pop(), 4);
-	EXPECT_EQ(test.pop(), 3);
-	EXPECT_EQ(test.pop(), 2);
+    EXPECT_EQ(test.pop(), 4);
+    EXPECT_EQ(test.pop(), 3);
+    EXPECT_EQ(test.pop(), 2);
 }
 
 TEST(stack_t, Text){
@@ -35,19 +35,19 @@ TEST(stack_t, Text){
     stack_t<int> test;
     int a = 2, b = 3, c = 4;
     test_summ.push_emplace(1,2,3);
-	test_summ.push_emplace(3,4,5);
+    test_summ.push_emplace(3,4,5);
     test.push(a);
     test.push(b);
     test.push(c);
     test.push(12);
 
-	EXPECT_EQ(test.head(), 12);
+    EXPECT_EQ(test.head(), 12);
     EXPECT_EQ(test.pop(), 12);
-	EXPECT_EQ(test.pop(), 4);
-	EXPECT_EQ(test.pop(), 3);
-	EXPECT_EQ(test.pop(), 2);
-	EXPECT_EQ(test_summ.pop(), 12);
-	EXPECT_EQ(test_summ.pop(), 6);
+    EXPECT_EQ(test.pop(), 4);
+    EXPECT_EQ(test.pop(), 3);
+    EXPECT_EQ(test.pop(), 2);
+    EXPECT_EQ(test_summ.pop(), 12);
+    EXPECT_EQ(test_summ.pop(), 6);
 }
 
 int main(int argc, char **argv) {
